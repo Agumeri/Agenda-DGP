63b0f6764638d64c6573b24aa466e47b58101cc2
Nacho-beta <nachobc82@gmail.com>
2021-11-10 20:19:53 +0100

    Add CRUD objeto, Inventario, gestInventario

--- backend/src/routes/dgp.js
+++ backend/src/routes/dgp.js
@@ -3,6 +3,10 @@ import { getUsuarios,  getUsuario, getUsuarioCount,  saveUsuario, deleteUsuario,
         createProfesor, getProfesores, getProfesor, getProfesorCount, updateProfesor, deleteProfesor,
         createAdmin, getAdmin, getAdminByID, getAdminCount, updateAdmin, deleteAdmin
        } from '../controllers/dgp'
+import {createInventario, getAllInventarios, getInventariosCount, getInventarioById, updateInventario, deleteInventario,
+        createGestInv, getAllGestInv, getCountGestInv, getByIdGestInv, updateGestInv, deleteGestInv,
+        createObjeto, getAllObjetos, getCountObjetos, getByIdObjetos, updateObjetos, deleteObjetos
+        } from '../controllers/inventario'
 
 const router = Router()
 
@@ -201,4 +205,187 @@ router.get('/admin/:id',getAdminByID)
  */
 router.delete('/admin/:id',deleteAdmin)
 
+/////////////////////
+//// INVENTARIO /////
+/////////////////////
+/**
+ * @swagger
+ * /inventario:
+ *  post:
+ *      summary: crea un nuevo inventario
+ *      tags: [Inventario] 
+ */
+ router.post('/inventario',createInventario);
+
+/**
+ * @swagger
+ * /inventario:
+ *  get:
+ *      summary: obtiene todos los inventarios
+ *      tags: [Inventario]
+ * 
+ */
+ router.get('/inventario',getAllInventarios)
+
+/**
+ * @swagger
+ * /inventario/count:
+ *  get:
+ *      summary: obtiene la cantidad de inventarios
+ *      tags: [Inventario]
+ * 
+ */
+ router.get('/inventario/count',getInventariosCount)
+
+/**
+ * @swagger
+ * /inventario/count:
+ *  get:
+ *      summary: obtiene la cantidad de inventarios
+ *      tags: [Inventario]
+ * 
+ */
+ router.get('/inventario/:id',getInventarioById)
+
+/**
+ * @swagger
+ * /inventario/id:
+ *  put:
+ *      summary: modifica un inventario
+ *      tags: [Inventario]
+ * 
+ */
+ router.put('/inventario/:id',updateInventario)
+
+/**
+ * @swagger
+ * /inventario/id:
+ *  delete:
+ *      summary: elimina un inventario
+ *      tags: [Inventario]
+ * 
+ */
+ router.delete('/inventario/:id',deleteInventario)
+
+//////////////////////////
+////GestionaInventario////
+//////////////////////////
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  post:
+ *      summary: crea un nuevo enlace entre comanda e inventario
+ *      tags: [gestiona_inventario] 
+ */
+ router.post('/gestiona_inventario', createGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+router.get('/gestiona_inventario', getAllGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+ router.get('/gestiona_inventario/count', getCountGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+ router.get('/gestiona_inventario/:id', getByIdGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  put:
+ *      summary: Cambia un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.put('/gestiona_inventario/:id', updateGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  put:
+ *      summary: Cambia un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.put('/gestiona_inventario/:id', updateGestInv)
+ 
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  delete:
+ *      summary: Elimina un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.delete('/gestiona_inventario/:id', deleteGestInv)
+/////////////////////
+////// OBJETOS //////
+/////////////////////
+/**
+ * @swagger
+ * /objeto:
+ *  post:
+ *      summary: crea un nuevo objeto
+ *      tags: [Objeto] 
+ */
+ router.post('/objetos',createObjeto);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener todos los objetos
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos',getAllObjetos);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener la cantidad de objetos
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos/count',getCountObjetos);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener un objeto por id
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos/:id',getByIdObjetos);
+
+/**
+ * @swagger
+ * /objetos:
+ *  put:
+ *      summary: Cambia los datos de un objeto dado
+ *      tags: [objetos] 
+ */
+ router.put('/objetos/:id', updateObjetos)
+
+/**
+ * @swagger
+ * /objetos:
+ *  delete:
+ *      summary: Elimina un enlace dado un id
+ *      tags: [objetos] 
+ */
+ router.delete('/objetos/:id', deleteObjetos)
 export default router;
--- backend/sripts/db.sql
+++ backend/sripts/db.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS dgp;
 
 USE dgp;
+-----------------
 -- Usuario table
 CREATE TABLE IF NOT EXISTS usuario(
     id INT NOT NULL AUTO_INCREMENT,
@@ -10,12 +11,11 @@ CREATE TABLE IF NOT EXISTS usuario(
     correo_electronico VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
-
 INSERT INTO usuario (nombre_usuario, contraseña, permisos, correo_electronico) VALUES 
     ('prueba1', 'prueba1', '0','prueba1'),
     ('pruebaAdmin','admin1234','0','pruebaAdmin@prueba'),
      ('prueba2', 'prueba2', '0','prueba2');
-
+------------------
 -- Profesor Table
 CREATE TABLE IF NOT EXISTS profesor(
     id_usuario INT NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE IF NOT EXISTS profesor(
     PRIMARY KEY (id_usuario,id_profesor),
     FOREIGN KEY (id_usuario) REFERENCES usuario(id)
 );
-
+------------------------
 -- Alumno_autoriza table
 CREATE TABLE IF NOT EXISTS alumno_tutoriza(
     id INT NOT NULL,
@@ -33,11 +33,10 @@ CREATE TABLE IF NOT EXISTS alumno_tutoriza(
     FOREIGN KEY (id) REFERENCES usuario(id),
     FOREIGN KEY (id_profesor) REFERENCES profesor(id_profesor)
 );
-
 INSERT INTO alumno_tutoriza (id, id_profesor) VALUES 
     ('1', '1'),
     ('2', '2');
-    
+--------------
 --Admin Table
 CREATE TABLE IF NOT EXISTS admin(
     id_admin VARCHAR(100) NOT NULL,
@@ -45,5 +44,35 @@ CREATE TABLE IF NOT EXISTS admin(
     PRIMARY KEY (id_usuario_inAdmin, id_admin),
     FOREIGN KEY (id_usuario_inAdmin) REFERENCES usuario(id)
 );
-
 INSERT INTO admin (id_admin, id_usuario_inAdmin) VALUES ('admin01','2');
+----------------
+--inventario Table
+CREATE TABLE IF NOT EXISTS inventario(
+    id_inventario varchar(100) NOT NULL PRIMARY KEY
+);
+INSERT INTO inventario(id_inventario) VALUES ('inventario1');
+----------------------------
+--gestiona_inventario Table
+CREATE TABLE IF NOT EXISTS gestiona_inventario(
+    id_gestiona_inventario varchar(100) NOT NULL UNIQUE,
+    id_tarea_inGestInv varchar(100) NOT NULL,
+    id_comanda_inGestInv varchar(100) NOT NULL,
+    id_inventario_inGestInv varchar(100) NOT NULL,
+    PRIMARY KEY (id_gestiona_inventario, id_tarea_inGestInv, id_comanda_inGestInv, id_inventario_inGestInv),
+    FOREIGN KEY (id_inventario_inGestInv) REFERENCES inventario(id_inventario)
+);
+INSERT INTO gestiona_inventario(id_gestiona_inventario, id_tarea_inGestInv, id_comanda_inGestInv, id_inventario_inGestInv) 
+VALUES ('gestiona_desdeSQL', 'tarea1','comanda1','inventario1');
+----------------
+--objeto Table
+CREATE TABLE IF NOT EXISTS objetos(
+    id_objeto varchar(100) NOT NULL,
+    id_inventario_inObjeto varchar(100) NOT NULL,
+    nombre_objeto VARCHAR(100),
+    cantidad_objetos INT,
+    categoria varchar(300),
+    PRIMARY KEY (id_objeto, id_inventario_inObjeto),
+    FOREIGN KEY (id_inventario_inObjeto) REFERENCES inventario(id_inventario)
+);
+INSERT INTO objetos(id_objeto, id_inventario_inObjeto, nombre_objeto, cantidad_objetos, categoria)
+VALUES ('objeto1', 'inventario1', 'boligrafo', '32', 'escritura');

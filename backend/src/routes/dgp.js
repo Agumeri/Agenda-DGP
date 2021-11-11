034a474ac600fa27107109c0dbdf7023c5acf5f1
Nacho-beta <nachobc82@gmail.com>
2021-11-11 13:21:10 +0100

    Add CRUD admin, inventario, gestInv, objeto

--- backend/src/routes/dgp.js
+++ backend/src/routes/dgp.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 import { getUsuarios,  getUsuario, getUsuarioCount,  saveUsuario, deleteUsuario, updateUsuario, 
         createProfesor, getProfesores, getProfesor, getProfesorCount, updateProfesor, deleteProfesor,
-        getAlumnoCount 
+        createAdmin, getAdmin, getAdminByID, getAdminCount, updateAdmin, deleteAdmin
        } from '../controllers/dgp'
+import {createInventario, getAllInventarios, getInventariosCount, getInventarioById, updateInventario, deleteInventario,
+        createGestInv, getAllGestInv, getCountGestInv, getByIdGestInv, updateGestInv, deleteGestInv,
+        createObjeto, getAllObjetos, getCountObjetos, getByIdObjetos, updateObjetos, deleteObjetos
+        } from '../controllers/inventario'
 
 const router = Router()
 
@@ -70,7 +74,7 @@ router.post('/usuario',saveUsuario)
  * @swagger
  * /usuario/id_user:
  *  delete:
- *      summary: elimina una tarea por id
+ *      summary: elimina una usuario por id
  *      tags: [Usuario]
  */
 router.delete('/usuario/:id',deleteUsuario)
@@ -129,7 +133,7 @@ router.get('/profesor/:id',getProfesor)
  * @swagger
  * /profesor/id_prof:
  *  put:
- *      summary: obtener un profesor de la lista de profesores
+ *      summary: actualiza un profesor de la lista de profesores
  *      tags: [Profesor]
  */
  router.put('/profesor/:id',updateProfesor)
@@ -144,19 +148,244 @@ router.get('/profesor/:id',getProfesor)
 router.delete('/profesor/:id',deleteProfesor)
 
 /////////////////////
-////// ALUMNO /////
+////// ADMIN /////
 /////////////////////
+ /**
+ * @swagger
+ * /admin:
+ *  post:
+ *      summary: crea un nuevo administrador
+ *      tags: [Admin]
+ */
+router.post('/admin',createAdmin);
+  
+/**
+ * @swagger
+ * /admin:
+ *  get:
+ *      summary: obtiene todos los admins del sistema
+ *      tags: [Admin]
+ * 
+ */
+router.get('/admin',getAdmin)
 
 /**
  * @swagger
- * /alumno/count:
+ * /admin/count:
+ *  post:
+ *      summary: Obtiene el numero de administradores
+ *      tags: [Admin]
+ */
+router.get('/admin/count',getAdminCount)
+
+/**
+ * @swagger
+ * /admin/id_admin:
  *  get:
- *      summary:Obtener el numero de alumnos del sistema
- *      tags: [Alumno]
+ *      summary: obtener un admin de la lista de administradores
+ *      tags: [Admin]
+ */
+router.get('/admin/:id',getAdminByID)
+
+/**
+ * @swagger
+ * /admin/id_admin:
+ *  put:
+ *      summary: actualiza un administrador de la lista
+ *      tags: [Admin]
+ */
+ router.put('/admin/:id',updateAdmin)
+
+/**
+ * @swagger
+ * /admin/id_admin:
+ *  delete:
+ *      summary: borrar un admin
+ *      tags: [Admin]
+ */
+router.delete('/admin/:id',deleteAdmin)
+
+/////////////////////
+//// INVENTARIO /////
+/////////////////////
+/**
+ * @swagger
+ * /inventario:
+ *  post:
+ *      summary: crea un nuevo inventario
+ *      tags: [Inventario] 
+ */
+ router.post('/inventario',createInventario);
+
+/**
+ * @swagger
+ * /inventario:
+ *  get:
+ *      summary: obtiene todos los inventarios
+ *      tags: [Inventario]
  * 
  */
- router.get('/alumno/count',getAlumnoCount)
+ router.get('/inventario',getAllInventarios)
 
+/**
+ * @swagger
+ * /inventario/count:
+ *  get:
+ *      summary: obtiene la cantidad de inventarios
+ *      tags: [Inventario]
+ * 
+ */
+ router.get('/inventario/count',getInventariosCount)
 
+/**
+ * @swagger
+ * /inventario/count:
+ *  get:
+ *      summary: obtiene la cantidad de inventarios
+ *      tags: [Inventario]
+ * 
+ */
+ router.get('/inventario/:id',getInventarioById)
 
+/**
+ * @swagger
+ * /inventario/id:
+ *  put:
+ *      summary: modifica un inventario
+ *      tags: [Inventario]
+ * 
+ */
+ router.put('/inventario/:id',updateInventario)
+
+/**
+ * @swagger
+ * /inventario/id:
+ *  delete:
+ *      summary: elimina un inventario
+ *      tags: [Inventario]
+ * 
+ */
+ router.delete('/inventario/:id',deleteInventario)
+
+//////////////////////////
+////GestionaInventario////
+//////////////////////////
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  post:
+ *      summary: crea un nuevo enlace entre comanda e inventario
+ *      tags: [gestiona_inventario] 
+ */
+ router.post('/gestiona_inventario', createGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+router.get('/gestiona_inventario', getAllGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+ router.get('/gestiona_inventario/count', getCountGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  get:
+ *      summary: Obtiene todos los enlaces entre comandas e inventarios
+ *      tags: [gestiona_inventario] 
+ */
+ router.get('/gestiona_inventario/:id', getByIdGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  put:
+ *      summary: Cambia un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.put('/gestiona_inventario/:id', updateGestInv)
+
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  put:
+ *      summary: Cambia un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.put('/gestiona_inventario/:id', updateGestInv)
+ 
+/**
+ * @swagger
+ * /gestiona_inventario:
+ *  delete:
+ *      summary: Elimina un enlace dado un id
+ *      tags: [gestiona_inventario] 
+ */
+ router.delete('/gestiona_inventario/:id', deleteGestInv)
+/////////////////////
+////// OBJETOS //////
+/////////////////////
+/**
+ * @swagger
+ * /objeto:
+ *  post:
+ *      summary: crea un nuevo objeto
+ *      tags: [Objeto] 
+ */
+ router.post('/objetos',createObjeto);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener todos los objetos
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos',getAllObjetos);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener la cantidad de objetos
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos/count',getCountObjetos);
+
+/**
+ * @swagger
+ * /objeto:
+ *  get:
+ *      summary: obtener un objeto por id
+ *      tags: [Objeto] 
+ */
+ router.get('/objetos/:id',getByIdObjetos);
+
+/**
+ * @swagger
+ * /objetos:
+ *  put:
+ *      summary: Cambia los datos de un objeto dado
+ *      tags: [objetos] 
+ */
+ router.put('/objetos/:id', updateObjetos)
+
+/**
+ * @swagger
+ * /objetos:
+ *  delete:
+ *      summary: Elimina un enlace dado un id
+ *      tags: [objetos] 
+ */
+ router.delete('/objetos/:id', deleteObjetos)
 export default router;

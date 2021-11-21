e434c29055a713639e36ee584158f8a0d9d782ec
AgLuque1 <79659314+AgLuque1@users.noreply.github.com>
2021-11-21 19:36:32 +0100

    Add files via upload
    Pantalla de Info de una tarea

---
+++ app/screens/InfoTarea.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View, Text} from "react-native"
+import Header from '../components/Header'
+
+const InfoTarea = () => {
+    return (
+        <View>
+            <Header></Header>
+            <div>Info de la tarea</div>
+        </View>
+    )
+}
+
+export default InfoTarea
